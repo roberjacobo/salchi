c74b0e2e16401615ee8feecc03c0456ed70822e7
Roberto Jacobo <robjacobox@gmail.com>
2025-12-12 17:16:55 -0600

    dark mode updates in progress

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './features/landing/components/hero/hero';
+import { About } from './features/landing/components/about/about';
 import { ThemeToggle } from './shared/components/theme-toggle/theme-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, ThemeToggle],
+  imports: [RouterOutlet, Hero, About, ThemeToggle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
